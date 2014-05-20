1a523e17bd724d8a244b0627d0f9654e392f2347
Duarte Miguel Tavares Barreira <ist164752@rnlmaster.rnl.ist.utl.pt>
2014-05-20 16:25:52 +0100

    Predicado check implementado

--- metaforms14.pl
+++ metaforms14.pl
@@ -28,6 +28,7 @@
 % no caso seguinte, do trioLeft, ha ainda que considerar o caso do middle
 %---------------------------------------------------------------------
 
+%Assim nao da warnings de singleton variable
 trioLeft(Peca, Linha, Coluna, Tabuleiro) :-
     (coloca(Peca, Linha, Coluna, Tabuleiro);
      coloca(Peca, Linha, middle, Tabuleiro)).
@@ -86,21 +87,12 @@ diagonalGrave(Peca, Linha, Coluna, Tabuleiro) :-
      coloca(Peca, Linha, middle, Tabuleiro);
      coloca(Peca, center, midlle, Tabuleiro)).
 
-%diagonalAguda(Peca, Linha, Coluna, Tabuleiro) :-
-%    coloca(Peca, Linha, Coluna, Tabuleiro).
-%diagonalAguda(Peca, Linha, Coluna, Tabuleiro) :-
-%    coloca(Peca, center, Coluna, Tabuleiro).
-%diagonalAguda(Peca, Linha, Coluna, Tabuleiro) :-
-%    coloca(Peca, Linha, middle, Tabuleiro).
-%diagonalAguda(Peca, Linha, Coluna, Tabuleiro) :-
-%    coloca(Peca, center, midlle, Tabuleiro).
-
-%Assim nao da warnings de singleton variable
 diagonalAguda(Peca, Linha, Coluna, Tabuleiro) :-
     (coloca(Peca, Linha, Coluna, Tabuleiro);
     coloca(Peca, center, Coluna, Tabuleiro);
     coloca(Peca, Linha, middle, Tabuleiro);
     coloca(Peca, center, midlle, Tabuleiro)).
+%
 %---------------------------------------------------------------------
 %
 %                         Predicado check
@@ -113,5 +105,44 @@ diagonalAguda(Peca, Linha, Coluna, Tabuleiro) :-
 % o tabuleiro, mas com as pecas em falta.
 %
 %---------------------------------------------------------------------
+check(TabuleiroIncompleto, TabuleiroCompleto) :-
+    encontraSol(TabuleiroIncompleto, TabuleiroCompleto, _).
 
-%check(TabuleiroIncompleto, TabuleiroCompleto)
+% Dado um tabuleiro com todas as pecas queremos encontrar as permutacoes das pecas
+% que podemos ter.
+% Depois so temos que eliminar as posicoes que nao nos interessam dadas as pistas que recebemos
+% Uma vez que no ficheiro resultados.txt apenas aparecia uma possivel solucao e o predicado
+% findall/3 devolve uma lista de possiveis resultados, nos extraimos o primeiro elemento
+% usando o predicado primeiro/2.
+encontraSol(TI, TC, Tabuleiro) :-
+    coloca(peca(triangulo, vermelho), top, left, Tabuleiro),
+    coloca(peca(triangulo, azul), top, middle, Tabuleiro),
+    coloca(peca(triangulo, amarelo), top, right, Tabuleiro),
+    coloca(peca(quadrado, vermelho), center, left, Tabuleiro),
+    coloca(peca(quadrado, azul), center, middle, Tabuleiro),
+    coloca(peca(quadrado, amarelo), center, right, Tabuleiro),
+    coloca(peca(circulo, vermelho), bottom, left, Tabuleiro),
+    coloca(peca(circulo, azul), bottom, middle, Tabuleiro),
+    coloca(peca(circulo, amarelo), bottom, right, Tabuleiro),
+    findall(TI, perm(Tabuleiro, TI), TX),
+    primeiro(TX, TC).
+
+% A lista L pode ser permutada numa lista que comeca com H e acaba com T se
+%  - L pode ser particionada em duas listas, V e [ H | U] (isto e, H e uma parte de L).
+%  - As listas V e U (A lista L toda excepto H) podem ser colocadas no fim da lista W.
+%  - T e uma permutacao de w.
+perm([], []).
+perm(L, [H | T]) :-
+    junta(V, [H|U], L),
+    junta(V, U, W), perm(W, T).
+
+
+% A expressao junta(X, Y, Z) afirma que a lista Z e o resultado de juntar
+% a lista X com a lista Y.
+% Pagina 312 do Livro de LP.
+junta([], L, L).
+junta([P | R], L1, [P | L2]) :- junta(R, L1, L2).
+
+%devolve o primeiro elemento da lista
+primeiro([], []).
+primeiro([X], X).
